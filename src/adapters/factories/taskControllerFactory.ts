9b655772fd395c260a61509ec1c926ecaa33ef20
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2025-06-29 09:23:46 -0300

    Refatoração

--- src/adapters/factories/taskControllerFactory.ts
+++ src/adapters/factories/taskControllerFactory.ts
@@ -2,12 +2,10 @@ import { DbAddTask } from "../../dataSources/db/dbAddTask";
 import { LogErrorMongoRepository } from "../../dataSources/db/repository/logErrorMongoRepository";
 import { TaskMongoRepository } from "../../dataSources/db/repository/taskMongoRepository";
 import { AddTaskController } from "../controllers/task/addTask";
-import { DateValidatorAdapter } from "../dateValidatorAdapter";
 import LogErrorControllerDecorator from "../decorators/logErrorControllerDecorator";
 import { addTaskValidationCompositeFactory } from "./addTaskValidationCompositeFactory";
 
 export  const taskControllerFactory = () => {
-  const dateValidatorAdapter = new DateValidatorAdapter();
   const taskMongoRepository = new TaskMongoRepository(); // repositório
   const dbAddTask = new DbAddTask(taskMongoRepository); // serviço
 
